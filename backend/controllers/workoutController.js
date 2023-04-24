b463c1e14e17205baaff3b7d8744f8837a003f6c
ROBILLIARD Grégory <gregory.robilliard@icloud.com>
2023-04-24 15:37:57 +0200

    backend

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,91 @@
+const Workout = require("../models/WorkoutModels");
+const mongoose = require("mongoose");
+
+// get all workouts
+const getWorkouts = async (req, res) => {
+  try {
+    const workouts = await Workout.find().sort({ createdAt: -1 });
+    res.status(200).json(workouts);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// get a single workout
+const getWorkout = async (req, res) => {
+  // get id from request
+  const { id } = req.params;
+
+  // check if id is valid on the server side
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+
+  // find workout by id
+  const workout = await Workout.findById(id);
+  if (!workout) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+  res.status(200).json(workout);
+};
+
+// create a new workout
+const createWorkout = async (req, res) => {
+  const { title, load, reps } = req.body;
+
+  // add document to database
+  try {
+    const workout = await Workout.create({ title, load, reps });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// delete a workout
+const deleteWorkout = async (req, res) => {
+  // get id from request
+  const { id } = req.params;
+
+  // check if id is valid on the server side
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+
+  // find workout by id and delete
+  const workout = await Workout.findOneAndDelete({ _id: id });
+
+  // return deleted workout
+  if (!workout) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+  res.status(200).json(workout);
+};
+
+// update a workout
+const updateWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  // check if id is valid on the server side
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+
+  // find workout by id and update
+  const workout = await Workout.findOneAndUpdate({ _id: id }, { ...req.body });
+
+  // return updated workout
+  if (!workout) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+
+  res.status(200).json(workout);
+};
+
+module.exports = {
+  createWorkout,
+  getWorkouts,
+  getWorkout,
+  deleteWorkout,
+  updateWorkout,
+};
---
+++ backend/routes/workouts.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const {
+  createWorkout,
+  getWorkout,
+  getWorkouts,
+  deleteWorkout,
+  updateWorkout,
+} = require("../controllers/workoutController");
+
+// Create a router
+const router = express.Router();
+
+// Create routes...
+
+//GET all workouts
+router.get("/", getWorkouts);
+
+//GET a single workout
+router.get("/:id", getWorkout);
+
+// POST a new workout
+router.post("/", createWorkout);
+
+// DELETE a workout
+router.delete("/:id", deleteWorkout);
+
+// UPDATE a workout
+router.patch("/:id", updateWorkout);
+
+// Export the router
+module.exports = router;
